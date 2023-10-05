dea75980acd3c587a0d9012fe030ff94e0c41c8c
Yifeng Zhu <yifeng.zhu@utexas.edu>
2023-10-05 15:50:41 -0500

    Add a missing file

---
+++ deoxys_vision/utils/camera_utils.py
@@ -0,0 +1,21 @@
+
+from easydict import EasyDict
+
+def assert_camera_ref_convention(camera_reference, 
+                       available_camera_types=["rs", "k4a"]):
+    
+    assert(isinstance(camera_reference, str)), "camera_id must be a string"
+    camera_id = int(camera_reference.split("_")[-1])
+    camera_type = camera_reference.split("_")[0]
+    assert(camera_type in available_camera_types), f"camera_type must be one of {available_camera_types}, got {camera_type}"
+    assert(camera_id >= 0), "camera id must be >= 0"
+
+def get_camera_info(camera_reference):
+    camera_id = int(camera_reference.split("_")[-1])
+    camera_type = camera_reference.split("_")[0]
+    camera_info = EasyDict({
+        "camera_id": camera_id,
+        "camera_type": camera_type,
+        "camera_name": f"camera_{camera_reference}"
+    })
+    return camera_info
